cddfb1937d4ec3387d5dbef11e1e44a53224c3b1
Greg Bowler <greg.bowler@g105b.com>
2015-02-24 15:53:04 +0000

    Begin adding logic

---
+++ src/Page/index.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Page;
+
+class Index extends \Gt\Page\Logic {
+
+public function go() {
+    // TODO 1: Make $taskList read from a persistent data source.
+    // (for now, just use an array).
+    $taskList = [];
+
+    // Handle user input first:
+    if(isset($_POST["action"])) {
+        switch($_POST["action"]) {
+        case "add":
+            // TODO 2: Implement add action.
+            break;
+
+        case "delete":
+            // TODO 3: Implement delete action.
+            break;
+
+        case "update":
+            // TODO 4: Implement update action.
+            break;
+
+        case "check":
+            // TODO 5: Implement check action.
+            break;
+        }
+    }
+
+    // Output task list:
+    foreach ($taskList as $task) {
+        // TODO 6: Output task to list on page.
+    }
+
+    // TODO 7: Output one extra empty task, for adding new ones.
+}
+
+}#
